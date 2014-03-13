a7bc530835c5bcd98b94ea30791e8f6b6f3d8650
Sue Zhu <suzhu@visa.com>
2014-03-12 17:43:06 -0700

    Do not store the login information in the sample code
    Do not store the login information in the sample code

--- CoffeeShopWebApp/Error.aspx.cs
+++ CoffeeShopWebApp/Error.aspx.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using CoffeeShopWebApp.Model;
 
 namespace CoffeeShopWebApp {
     public partial class Error : System.Web.UI.Page {
-        protected void Page_Load(object sender, EventArgs e) {
-
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lblMessage.Text = ((CoffeeOrder) Session["order"]).OrderMessage;
         }
     }
 }
