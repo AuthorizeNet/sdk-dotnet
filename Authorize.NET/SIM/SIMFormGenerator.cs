2e121104520a8bdbc598e32fc4be1a4ccfef3f35
Sue Zhu <suzhu@visa.com>
2014-05-08 12:44:27 -0700

    Bug Fix for DPM and SIM
    Correct the html closing tag.
    
    Thanks to sweeperq's finding.
    https://github.com/AuthorizeNet/sdk-dotnet/issues/24

--- Authorize.NET/SIM/SIMFormGenerator.cs
+++ Authorize.NET/SIM/SIMFormGenerator.cs
@@ -25,17 +25,17 @@ namespace AuthorizeNet
                 formAction = Gateway.TEST_URL;
 
             sbForm.Append("<form action = '" + formAction + "' method = 'post'>\n");
-            sbForm.Append("\t\t<input type = 'hidden' name = 'x_fp_hash' value = '" + fingerPrint + "' \\>\n");
-            sbForm.Append("\t\t<input type = 'hidden' name = 'x_fp_sequence' value = '" + seq + "'\\>\n");
-            sbForm.Append("\t\t<input type = 'hidden' name = 'x_fp_timestamp' value = '" + stamp + "' \\>\n");
-            sbForm.Append("\t\t<input type = 'hidden' name = 'x_login' value = '" + apiLogin + "' \\>\n");
-            sbForm.Append("\t\t<input type = 'hidden' name = 'x_amount' value = '" + amount + "' \\>\n");
-            sbForm.Append("\t\t<input type = 'hidden' name = 'x_show_form' value = 'PAYMENT_FORM' \\>\n");
+            sbForm.Append("\t\t<input type = 'hidden' name = 'x_fp_hash' value = '" + fingerPrint + "' />\n");
+            sbForm.Append("\t\t<input type = 'hidden' name = 'x_fp_sequence' value = '" + seq + "' />\n");
+            sbForm.Append("\t\t<input type = 'hidden' name = 'x_fp_timestamp' value = '" + stamp + "' />\n");
+            sbForm.Append("\t\t<input type = 'hidden' name = 'x_login' value = '" + apiLogin + "' />\n");
+            sbForm.Append("\t\t<input type = 'hidden' name = 'x_amount' value = '" + amount + "' />\n");
+            sbForm.Append("\t\t<input type = 'hidden' name = 'x_show_form' value = 'PAYMENT_FORM' />\n");
 
             if ((!string.IsNullOrEmpty(returnUrl)) && (returnUrl.Trim().Length > 0))
             {
-                sbForm.Append("\t\t<input type = 'hidden' name = 'x_relay_url' value = '" + returnUrl + "' \\>\n");
-                sbForm.Append("\t\t<input type = 'hidden' name = 'x_relay_response' value = 'TRUE' \\>\n");
+                sbForm.Append("\t\t<input type = 'hidden' name = 'x_relay_url' value = '" + returnUrl + "' />\n");
+                sbForm.Append("\t\t<input type = 'hidden' name = 'x_relay_response' value = 'TRUE' />\n");
             }
 
             return sbForm.ToString();
