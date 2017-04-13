b6223eccfbf21c62685e675bcc3753c51d794e67
bselvara <bselvara@visa.com>
2017-04-13 17:00:57 +0530

    SDK updates

---
+++ Authorize.NET/Api/Controllers/getTransactionListForCustomerController.cs
@@ -0,0 +1,30 @@
+﻿namespace AuthorizeNet.Api.Controllers
+{
+    using System;
+    using AuthorizeNet.Api.Contracts.V1;
+    using AuthorizeNet.Api.Controllers.Bases;
+
+#pragma warning disable 1591
+    public class getTransactionListForCustomerController : ApiOperationBase<getTransactionListForCustomerRequest, getTransactionListResponse> {
+
+	    public getTransactionListForCustomerController(getTransactionListForCustomerRequest apiRequest) : base(apiRequest) {
+	    }
+
+	    override protected void ValidateRequest() {
+            var request = GetApiRequest();
+		
+		    //validate required fields		
+		    //if ( 0 == request.SearchType) throw new ArgumentException( "SearchType cannot be null");
+		    //if ( null == request.Paging) throw new ArgumentException("Paging cannot be null");
+		
+		    //validate not-required fields		
+	    }
+
+        protected override void BeforeExecute()
+        {
+            var request = GetApiRequest();
+            RequestFactoryWithSpecified.getTransactionListForCustomerRequest(request);
+        }
+    }
+#pragma warning restore 1591
+}
