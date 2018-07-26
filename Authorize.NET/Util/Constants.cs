1e2e6fec009a25402b35f9c31371a4ba5221bdf2
andreabarry1 <36756442+andreabarry1@users.noreply.github.com>
2018-07-26 17:53:57 -0600

    Revert "Update to 1.9.5"

--- Authorize.NET/Util/Constants.cs
+++ Authorize.NET/Util/Constants.cs
@@ -26,7 +26,7 @@ namespace AuthorizeNet.Util
         public const int HttpConnectionDefaultTimeout = 30000;
         public const int HttpReadWriteDefaultTimeout = 30000;
 
-        public const string SDKVersion = "1.9.5";
+        public const string SDKVersion = "1.9.4";
 
     }
 #pragma warning restore 1591
