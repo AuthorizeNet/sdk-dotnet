2d0236a72226ad9418b5a796caab693419ae8bd2
Ashutosh Singh <ashtru@users.noreply.github.com>
2018-08-02 15:42:52 +0530

    Update AssemblyInfo.cs

--- Authorize.NET/Properties/AssemblyInfo.cs
+++ Authorize.NET/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AuthorizeNet Client SDK")]
+[assembly: AssemblyTitle("Official .NET SDK for Authorize.Net")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Visa Inc")]
-[assembly: AssemblyProduct("AuthorizeNet Client SDK")]
+[assembly: AssemblyProduct("Official .NET SDK for Authorize.Net")]
 [assembly: AssemblyCopyright("Copyright © Visa Inc 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
